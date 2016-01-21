4deb05499ca5f2df7ee9762b775cc514a203d500
Abby <abby@Bitmaker-6.local>
2016-01-21 12:23:28 -0500

    added friendly ID gem to automatically generate slugs for blog post

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,18 @@
 class Entry < ActiveRecord::Base
+
+
+	extend FriendlyId
+	friendly_id :title, use: [:slugged, :finders]
+
+	validates :slug, presence: true,  uniqueness: true
+
+
+	 scope :published, -> { where('published_at <= ?', Time.zone.now) }
+
+	def to_param
+    	slug
+	end
+
+
+
 end
